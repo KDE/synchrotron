bccf32550606887e7c62e7f7b5270e9330b3daed
Aaron Seigo <aseigo@kde.org>
2010-12-31 14:55:42 -0800

    first draft of db

---
+++ sql/ddl.sql
@@ -0,0 +1,30 @@
+-- drop sequence seq_providerIds;
+CREATE SEQUENCE seq_providerIds;
+
+-- drop table providers;
+CREATE TABLE providers
+(
+    id          INT         PRIMARY KEY DEFAULT nextval('seq_providerIds'),
+    name        TEXT        NOT NULL,
+    typename    TEXT        -- FIXME: i18n
+);
+
+-- drop sequence seq_contentIds;
+CREATE SEQUENCE seq_contentIds;
+
+-- DROP TABLE content
+CREATE TABLE content
+(
+    id          INT         PRIMARY KEY DEFAULT nextval('seq_providerIds'),
+    provider    INT         NOT NULL REFERENCES providers(id) ON DELETE CASCADE,
+    created     TIMESTAMP   NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated     TIMESTAMP   NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    downloads   INT         NOT NULL DEFAULT 0,
+    version     TEXT,
+    author      TEXT,
+    homepage    TEXT,
+    preview     TEXT,
+    name        TEXT        NOT NULL, -- FIXME: i18n
+    description TEXT -- FIXME: i18n
+);
+
