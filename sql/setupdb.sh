6e7a831442cd6ede124a432c9ca1544c80c85ab9
Aaron Seigo <aseigo@kde.org>
2011-01-16 14:09:48 -0800

    needs to run through psql

--- sql/setupdb.sh
+++ sql/setupdb.sh
@@ -5,6 +5,6 @@
 createuser -R -S -D synchrotron
 createuser -R -S -D synchrotron_ro
 createdb -O synchrotron synchrotron
-./ddl.sql
-./procedures.plsql
+psql -U synchrotron synchrotron -f ddl.sql
+psql -U synchrotron synchrotron -f procedures.plsql
 
