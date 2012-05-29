32ed44cf1daeaad7d05a9cb900a0c35037116bf7
Aaron Seigo <aseigo@kde.org>
2012-05-29 18:36:24 +0200

    fix setup script

--- sql/setupdb.sh
+++ sql/setupdb.sh
@@ -5,6 +5,6 @@
 createuser -R -S -D synchrotron
 createuser -R -S -D synchrotron_ro
 createdb -O synchrotron synchrotron
-psql -U synchrotron synchrotron -f ddl.sql
+psql -U synchrotron synchrotron -f postgres.sql
 psql -U synchrotron synchrotron -f procedures.plsql
 
